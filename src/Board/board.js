ba05f10caf0b190315403b10cbfc13a3569d7778
Hasnain Iqbal <60136067+shirsho1106@users.noreply.github.com>
2022-08-23 12:56:33 +0600

    boardstate in string done, eval function to go

--- src/Board/board.js
+++ src/Board/board.js
@@ -154,16 +154,66 @@ const Board = () => {
 			squares[num] = 'b';
 			setCells(squares)
 			setTurn('r');
+			//boardlines(squares);
 			comMove(squares);
 			setTurn('b');
 		} else {
 			squares[num] = 'r';
 			setTurn('b');
+			// boardlines(squares);
 		}
 
 		checkForWinner(squares);
 		setCells(squares);
 	};
+
+	const boardlines = (squares) => {
+		let row_count = 10;
+		let col_count = 10;
+		let rows = new Array(10)
+		let cols = new Array(10)
+		let ldgs = new Array(19)
+		let rdgs = new Array(19)
+		let box = [...squares]
+		
+		for(let i = 0 ; i < 100 ; i++) if(box[i]=='') box[i] = '-'
+		for (let i = 0; i < row_count; i++) {
+			rows[i] = new String
+			cols[i] = new String
+			for (let j = 0; j < col_count; j++) {
+				rows[i] += box[i*col_count+j]
+				cols[i] += box[i+col_count*j]
+			}
+		}
+
+		let dgsize = 0, start = 0;
+		for(let i = 0 ; i < 19 ; i++){
+			start = i;
+			if(i>9) start = (i-9)*9+i;
+			if(i<10) dgsize = i+1;
+			else dgsize--;
+			ldgs[i] = new String
+			for(let j = 0 ; j < dgsize ; j++){
+				ldgs[i] += box[start];
+				start+=9
+			}
+		}
+		dgsize = 0; start = 0;
+		for(let i = 0 ; i < 19 ; i++){
+			start = 9-i;
+			if(i>9) start = (i-10)*10+10;
+			if(i<10) dgsize = i+1;
+			else dgsize--;
+			rdgs[i] = new String
+			for(let j = 0 ; j < dgsize ; j++){
+				rdgs[i] += box[start];
+				start+=11
+			}
+		}
+		for(let i = 0 ; i < 19 ; i++){
+			console.log("rows",rows[i],"cols",cols[i],"ldgs",ldgs[i],"rdgs",rdgs[i]);
+		}
+	}
 
 	const handleRestart = () => {
 		setWinner('');
--- src/ComMove.js
+++ src/ComMove.js
@@ -20,8 +20,6 @@ export const comMove = (squares) => {
     bestMove = -1;
 }
 
-
-
 // const checkForWinner = (squares) => {
 
 //     const [winner, setWinner] = useState('');
@@ -116,122 +114,128 @@ export const comMove = (squares) => {
 //     }
 // };
 
+// function checkForWinner(board){
+//     let squares = [...board]
+//     const row_count = 10;
+// 	const col_count = 10;
+// 	const win_count = 5;
+
+//     let redrow = 0;
+//     let blackrow = 0;
+//     let redcol = 0;
+//     let blackcol = 0;
+//     let reddg = 0;
+//     let blackdg = 0;
+
+//     for(var i = 0 ; i < row_count ; i++){
+//         for(var j = 0 ; j < col_count ; j++){
+//             if(squares[i*col_count+j]==='r') redrow++;
+//             else {
+//                 redrow = 0;
+//             }
+//             if(squares[j*row_count+i]==='r') redcol++;
+//             else {
+//                 redcol = 0;
+//             }
+//             // console.log(i+" ind"+(i*10+j)+"red"+red+"black"+black);
+//             if(redrow===win_count || redcol===win_count){
+//                 return 'r';
+//             }
+//         }
+//         redrow = 0; blackrow = 0;
+//         redcol = 0; blackcol = 0;
+//     }
+//     for(var i = 0 ; i < row_count ; i++){
+//         for(var j = 0 ; j < col_count ; j++){
+//             if(squares[i*col_count+j]==='r') blackrow++;
+//             else {
+//                 blackrow = 0;
+//             }
+//             if(squares[j*row_count+i]==='r') blackcol++;
+//             else {
+//                 blackcol = 0;
+//             }
+//             // console.log(i+" ind"+(i*10+j)+"red"+red+"black"+black);
+//             if(blackrow===win_count || blackcol===win_count){
+//                 return 'b';
+//             }
+//         }
+//         redrow = 0; blackrow = 0;
+//         redcol = 0; blackcol = 0;
+//     }
+
+//     for (var k = 0; k <= 2 * (row_count - 1); ++k) {
+        
+//         for (var y = col_count - 1; y >= 0; --y) {
+//             var x = k - y;
+//             if (x >= 0 && x < row_count) {
+//                 // console.log(y*10+x);
+//                 if(squares[y*col_count+x]==='r') reddg++;
+//                 else {
+//                     reddg = 0;
+//                 }
+//                 if(reddg===win_count){
+//                     return 'r'
+//                 }
+//             }
+//         }
+//         reddg = 0; blackdg = 0;
+//     }
+//     for (var k = 0; k <= 2 * (row_count - 1); ++k) {
+        
+//         for (var y = col_count - 1; y >= 0; --y) {
+//             var x = k - y;
+//             if (x >= 0 && x < row_count) {
+//                 // console.log(y*10+x);
+//                 if(squares[y*col_count+x]==='b') blackdg++;
+//                 else {
+//                     blackdg = 0;
+//                 }
+//                 if(blackdg===win_count){
+//                     return 'b'
+//                 }
+//             }
+//         }
+//         reddg = 0; blackdg = 0;
+//     }
+//     reddg = 0; blackdg = 0;
+
+//     for (var k = 0; k <= 2 * (row_count - 1); ++k) {
+//         for (var y = col_count - 1; y >= 0; --y) {
+//             var x = k - (col_count - y);
+//             if (x >= 0 && x < row_count) {
+//                 if(squares[y*col_count+x]==='r') reddg++;
+//                 else {
+//                     reddg = 0;
+//                 }
+//                 if(reddg===win_count){
+//                     return 'r'
+//                 }
+//             }
+//         }
+//     }
+//     for (var k = 0; k <= 2 * (row_count - 1); ++k) {
+//         for (var y = col_count - 1; y >= 0; --y) {
+//             var x = k - (col_count - y);
+//             if (x >= 0 && x < row_count) {
+//                 if(squares[y*col_count+x]==='b') blackdg++;
+//                 else {
+//                     blackdg = 0;
+//                 }
+//                 if(blackdg===win_count){
+//                     return 'b'
+//                 }
+//             }
+//         }
+//     }
+
+//     return ''
+// }
+
 function checkForWinner(board){
-    let squares = [...board]
-    const row_count = 10;
-	const col_count = 10;
-	const win_count = 5;
-
-    let redrow = 0;
-    let blackrow = 0;
-    let redcol = 0;
-    let blackcol = 0;
-    let reddg = 0;
-    let blackdg = 0;
-
-    for(var i = 0 ; i < row_count ; i++){
-        for(var j = 0 ; j < col_count ; j++){
-            if(squares[i*col_count+j]==='r') redrow++;
-            else {
-                redrow = 0;
-            }
-            if(squares[j*row_count+i]==='r') redcol++;
-            else {
-                redcol = 0;
-            }
-            // console.log(i+" ind"+(i*10+j)+"red"+red+"black"+black);
-            if(redrow===win_count || redcol===win_count){
-                return 'r';
-            }
-        }
-        redrow = 0; blackrow = 0;
-        redcol = 0; blackcol = 0;
-    }
-    for(var i = 0 ; i < row_count ; i++){
-        for(var j = 0 ; j < col_count ; j++){
-            if(squares[i*col_count+j]==='r') blackrow++;
-            else {
-                blackrow = 0;
-            }
-            if(squares[j*row_count+i]==='r') blackcol++;
-            else {
-                blackcol = 0;
-            }
-            // console.log(i+" ind"+(i*10+j)+"red"+red+"black"+black);
-            if(blackrow===win_count || blackcol===win_count){
-                return 'b';
-            }
-        }
-        redrow = 0; blackrow = 0;
-        redcol = 0; blackcol = 0;
-    }
-
-    for (var k = 0; k <= 2 * (row_count - 1); ++k) {
-        
-        for (var y = col_count - 1; y >= 0; --y) {
-            var x = k - y;
-            if (x >= 0 && x < row_count) {
-                // console.log(y*10+x);
-                if(squares[y*col_count+x]==='r') reddg++;
-                else {
-                    reddg = 0;
-                }
-                if(reddg===win_count){
-                    return 'r'
-                }
-            }
-        }
-        reddg = 0; blackdg = 0;
-    }
-    for (var k = 0; k <= 2 * (row_count - 1); ++k) {
-        
-        for (var y = col_count - 1; y >= 0; --y) {
-            var x = k - y;
-            if (x >= 0 && x < row_count) {
-                // console.log(y*10+x);
-                if(squares[y*col_count+x]==='b') blackdg++;
-                else {
-                    blackdg = 0;
-                }
-                if(blackdg===win_count){
-                    return 'b'
-                }
-            }
-        }
-        reddg = 0; blackdg = 0;
-    }
-    reddg = 0; blackdg = 0;
-
-    for (var k = 0; k <= 2 * (row_count - 1); ++k) {
-        for (var y = col_count - 1; y >= 0; --y) {
-            var x = k - (col_count - y);
-            if (x >= 0 && x < row_count) {
-                if(squares[y*col_count+x]==='r') reddg++;
-                else {
-                    reddg = 0;
-                }
-                if(reddg===win_count){
-                    return 'r'
-                }
-            }
-        }
-    }
-    for (var k = 0; k <= 2 * (row_count - 1); ++k) {
-        for (var y = col_count - 1; y >= 0; --y) {
-            var x = k - (col_count - y);
-            if (x >= 0 && x < row_count) {
-                if(squares[y*col_count+x]==='b') blackdg++;
-                else {
-                    blackdg = 0;
-                }
-                if(blackdg===win_count){
-                    return 'b'
-                }
-            }
-        }
-    }
-
-    return ''
+    let red = "rrrrr";
+    let black = "bbbbb";
+    // pass
 }
 
 function sequence(board, player){
@@ -301,8 +305,6 @@ function sequence(board, player){
     return Math.max(row,col,ldg,rdg)
 }
 
-
-
 function matcher(board){
     let redpatterns = [
         "rrrrr",
@@ -350,22 +352,32 @@ function matcher(board){
 
     let row_count = 10;
     let col_count = 10;
-    let rows = []
-    let cols = []
-    let dgs = []
+    let boardstate = null
     let redpoint = 0, blackpoint = 0;
 
     //console.log("match");
 
-    for(let i = 0 ; i < row_count ; i++){
-        for(let j = 0 ; j < col_count ; j++){
-            if(board[i*col_count+j]=='') rows[i]+='-'
-            else rows[i] += board[i*col_count+j]
-        }
-        for(let j = 0 ; j < 12 ; j++){
-            if(rows[i].includes(redpatterns[j])) redpoint += scores[j]
-            if(rows[i].includes(blackpatterns[j])) blackpoint += scores[j]
+    boardstate = boardlines(board)
+
+    for(let i = 0 ; i < 19 ; i++){
+        for (let j = 0; j < redpatterns.length; j++) {
+            if(i<10) if(boardstate.rows[i].includes(redpatterns[j])) redpoint += scores[j];
+            if(i<10) if(boardstate.cols[i].includes(redpatterns[j])) redpoint += scores[j];
+            if(boardstate.ldgs[i].includes(redpatterns[j])) redpoint += scores[j];
+            if(boardstate.rdgs[i].includes(redpatterns[j])) redpoint += scores[j];
+            if(i<10) if(boardstate.rows[i].includes(redpatterns[j].split("").reverse().join(""))) redpoint += scores[j];
+            if(i<10) if(boardstate.cols[i].includes(redpatterns[j].split("").reverse().join(""))) redpoint += scores[j];
+            if(boardstate.ldgs[i].includes(redpatterns[j].split("").reverse().join(""))) redpoint += scores[j];
+            if(boardstate.rdgs[i].includes(redpatterns[j].split("").reverse().join(""))) redpoint += scores[j];
             
+            if(i<10) if(boardstate.rows[i].includes(blackpatterns[j])) blackpoint += scores[j];
+            if(i<10) if(boardstate.cols[i].includes(blackpatterns[j])) blackpoint += scores[j];
+            if(boardstate.ldgs[i].includes(blackpatterns[j])) blackpoint += scores[j];
+            if(boardstate.rdgs[i].includes(blackpatterns[j])) blackpoint += scores[j];
+            if(i<10) if(boardstate.rows[i].includes(blackpatterns[j].split("").reverse().join(""))) blackpoint += scores[j];
+            if(i<10) if(boardstate.cols[i].includes(blackpatterns[j].split("").reverse().join(""))) blackpoint += scores[j];
+            if(boardstate.ldgs[i].includes(blackpatterns[j].split("").reverse().join(""))) blackpoint += scores[j];
+            if(boardstate.rdgs[i].includes(blackpatterns[j].split("").reverse().join(""))) blackpoint += scores[j];
         }
     }
 
@@ -376,8 +388,8 @@ function matcher(board){
 }
 
 function evaluate(board){
-    return Math.max(sequence(board,'r'),sequence(board,'b'))
-    //return matcher(board)
+    //return Math.max(sequence(board,'r'),sequence(board,'b'))
+    return matcher(board)
 }
 
 function minimax(board, maxPlayer, depth, alpha, beta){
@@ -416,4 +428,58 @@ function minimax(board, maxPlayer, depth, alpha, beta){
     }
 
     return score;
+}
+
+function boardlines(squares){
+    let row_count = 10;
+    let col_count = 10;
+    let rows = new Array(10)
+    let cols = new Array(10)
+    let ldgs = new Array(19)
+    let rdgs = new Array(19)
+    let box = [...squares]
+    
+    for(let i = 0 ; i < 100 ; i++) if(box[i]=='') box[i] = '-'
+    for (let i = 0; i < row_count; i++) {
+        rows[i] = new String
+        cols[i] = new String
+        for (let j = 0; j < col_count; j++) {
+            rows[i] += box[i*col_count+j]
+            cols[i] += box[i+col_count*j]
+        }
+    }
+
+    let dgsize = 0, start = 0;
+    for(let i = 0 ; i < 19 ; i++){
+        start = i;
+        if(i>9) start = (i-9)*9+i;
+        if(i<10) dgsize = i+1;
+        else dgsize--;
+        ldgs[i] = new String
+        for(let j = 0 ; j < dgsize ; j++){
+            ldgs[i] += box[start];
+            start+=9
+        }
+    }
+    dgsize = 0; start = 0;
+    for(let i = 0 ; i < 19 ; i++){
+        start = 9-i;
+        if(i>9) start = (i-10)*10+10;
+        if(i<10) dgsize = i+1;
+        else dgsize--;
+        rdgs[i] = new String
+        for(let j = 0 ; j < dgsize ; j++){
+            rdgs[i] += box[start];
+            start+=11
+        }
+    }
+    // for(let i = 0 ; i < 19 ; i++){
+    //     console.log("rows",rows[i],"cols",cols[i],"ldgs",ldgs[i],"rdgs",rdgs[i]);
+    // }
+    return {
+        rows:rows,
+        cols:cols,
+        ldgs:ldgs,
+        rdgs:rdgs
+    }
 }
